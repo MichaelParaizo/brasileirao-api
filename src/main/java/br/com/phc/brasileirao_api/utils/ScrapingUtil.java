259a9d0f7d5085f6acd4e01ddad43c007e3f49da
Michael <michael-couto@hotmail.com>
2025-03-18 21:16:38 -0300

    Criacao do dto

---
+++ src/main/java/br/com/phc/brasileirao_api/utils/ScrapingUtil.java
@@ -0,0 +1,15 @@
+package br.com.phc.brasileirao_api.utils;
+
+import br.com.phc.brasileirao_api.dto.PartidaGoogleDTO;
+
+public class ScrapingUtil {
+
+    private static final String BASE_URL_GOOGLE = "https://www.google.com.br/search?q=";
+    private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
+
+    public static void main(String[] args) {
+
+        PartidaGoogleDTO partidaGoogleDTO = new PartidaGoogleDTO();
+
+    }
+}
